e8efcb02e20202c58b561fe60155a0c42c5bc036
NIZAMMMMM <nizammuhammed055@gmail.com>
2024-11-18 10:01:37 +0530

    footer removed

--- frontend/src/routes/components/Footer.js
+++ frontend/src/routes/components/Footer.js
@@ -3,9 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-slate-200'>
-      <div className='container mx-auto p-4'>
-      <p className='text-center font-bold'>NEVER GIVE UP</p>
-      </div>
+
     </footer>
   )
 }
